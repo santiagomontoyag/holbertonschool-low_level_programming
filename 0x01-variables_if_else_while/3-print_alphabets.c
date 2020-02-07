a2136ecd3f76843f1135592ac199c98413a3cc45
santiago <1669@holbertonschool.com>
2020-02-07 00:55:45 +0000

    answer

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	char b, c;
 
-	for (b = 'a'; b < = 'z'; b++)
+	for (b = 'a'; b <= 'z'; b++)
 		putchar(b);
-	for (c = 'A'; c < = 'z'; c++)
+	for (c = 'A'; c <= 'z'; c++)
 		putchar(c);
 	putchar('\n');
 	return (0);
