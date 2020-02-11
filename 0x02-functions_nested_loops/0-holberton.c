cfc7cdbdd1fbd660291b063957b231caf57673b7
santiago <1669@holbertonschool.com>
2020-02-11 14:14:35 +0000

    answer

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,14 @@
 #include "holberton.h"
+/**
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
 int main(void)
 {
 	_putchar('H');
