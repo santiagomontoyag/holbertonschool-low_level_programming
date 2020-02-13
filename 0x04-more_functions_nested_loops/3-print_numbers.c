d53b83302f549cde67eaecf8ac35de9969507f09
santiago <1669@holbertonschool.com>
2020-02-13 19:28:06 +0000

    answer

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - function for printing numbers
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int s;
+	for (s = '0'; s <= '9'; s++)
+		_putchar (s);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,8 +1,10 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
