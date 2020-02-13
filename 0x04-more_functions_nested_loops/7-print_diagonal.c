a4619fe00d8cc724358d49b19e7507c302dcac81
santiago <1669@holbertonschool.com>
2020-02-13 23:23:59 +0000

    answer

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_diagonal - function for printing diagonals
+ * @n: number of diagonals
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int s, m;
+
+	if (n <= 0)
+		_putchar('\n');
+	
+	else
+	{
+		for (s = 1; s <= n; s++)
+		{
+			for (m =  1; m <= s; m++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+
+}
